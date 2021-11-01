6367f0dfbc9e374f106e6f86f8785fff30e970b8
Qkiii <rukat15@gmail.com>
2021-11-01 15:22:23 +0100

    Init Android project

---
+++ app/src/main/java/com/qky/mvvmjumpstart/ui/main/MainFragment.kt
@@ -0,0 +1,30 @@
+package com.qky.mvvmjumpstart.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.qky.mvvmjumpstart.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/qky/mvvmjumpstart/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.qky.mvvmjumpstart.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
